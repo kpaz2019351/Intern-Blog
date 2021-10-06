bdec6d624f211b78f0c6a4c7bd8ed604ea9d706f
Kevin <kpaz-2019351@kinal.edu.gt>
2021-10-06 15:05:17 -0600

    Estilo

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.shortcuts import reverse
 
 class User(AbstractUser):
     pass
@@ -19,6 +20,20 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={
+            'slug': self.slug
+        })
+
+    @property
+    def comments(self):
+        return self.comment_set.all()
+
+    @property
+    def get_comment_count(self):
+        return self.comment_set.all().count()
+
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -27,6 +42,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.user.username
+
+   
 
 class PostView(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, PostView, Like, Comment
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 class PostListView(ListView):
     model = Post
 
 class PostDetailView(DetailView):
     model = Post
+
+    def post(self, *args, **kwargs):
+        form = CommentForm(self.request.POST)
+        if form.is_valid():
+            post = self.get_object()
+            comment = form.instance
+            comment.user = self.request.user
+            comment.post = post
+            comment.save()
+            return redirect("detail", slug=post.slug)
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context.update({
+                'form': CommentForm()
+            })
+            return context
+            
 
 class PostCreateView(CreateView):
     form_class = PostForm
